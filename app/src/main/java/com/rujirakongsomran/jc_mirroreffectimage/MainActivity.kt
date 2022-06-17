e2c3e61261077b31143a2809c89a511c5221aebe
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-06-17 16:32:04 +0700

    Inverted image

--- app/src/main/java/com/rujirakongsomran/jc_mirroreffectimage/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_mirroreffectimage/MainActivity.kt
@@ -3,13 +3,19 @@ package com.rujirakongsomran.jc_mirroreffectimage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_mirroreffectimage.ui.theme.JC_MirrorEffectImageTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +36,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Mirror(content: @Composable () -> Unit) {
+    Column {
+        content()
+        Box(
+            modifier = Modifier
+                .graphicsLayer {
+                    alpha = 0.99f
+                    rotationZ = 180f
+                }
+        ) {
+            content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JC_MirrorEffectImageTheme {
-        Greeting("Android")
+        Mirror {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .clip(RoundedCornerShape(24.dp)),
+                painter = painterResource(id = R.drawable.grain),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
